b5de1d5e75e686db7b88bc6e1f37756199a92a0f
Stella Zevio <stella.zevio@etu.umontpellier.fr>
2017-03-24 16:07:23 +0100

    correction script
    precision/rappel inversés sur l’éval des CUI

--- eval_quaero.sh
+++ eval_quaero.sh
@@ -11,15 +11,15 @@ corpusname=`basename $corpus`
 corpusdir=`dirname $corpus`
 resdir="$ontologies"_em"$expandMappings"_"$format"_"$corpusname"`basename $corpusdir`
 
-rm -rf results/$resdir
+#rm -rf results/$resdir
 
-java -jar target/bpeval.jar $corpus results/$resdir $format $expandMappings ${@:4:$#} > /dev/null
+#java -jar target/bpeval.jar $corpus results/$resdir $format $expandMappings ${@:4:$#} > /dev/null
 
 res1="$corpusname"`basename $corpusdir`_"$ontologies"_CUI_em"$expandMappings"_"$format"_EM.txt
-java -cp ../QUAERO_FrenchMed/eval/BRATeval-2015-04-24.jar au.com.nicta.csp.brateval.CompareNotesEHealth $corpus results/$resdir true >> results/$res1
+java -cp ../QUAERO_FrenchMed/eval/BRATeval-2015-04-24.jar au.com.nicta.csp.brateval.CompareNotesEHealth results/$resdir  $corpus true >> results/$res1
 
 res2="$corpusname"`basename $corpusdir`_"$ontologies"_CUI_em"$expandMappings"_"$format"_PM.txt
-java -cp ../QUAERO_FrenchMed/eval/BRATeval-2015-04-24.jar au.com.nicta.csp.brateval.CompareNotesEHealth $corpus results/$resdir false >> results/$res2
+java -cp ../QUAERO_FrenchMed/eval/BRATeval-2015-04-24.jar au.com.nicta.csp.brateval.CompareNotesEHealth results/$resdir  $corpus false >> results/$res2
 
 
 res3="$corpusname"`basename $corpusdir`_"$ontologies"_Semantic_em"$expandMappings"_"$format"_EM.txt
